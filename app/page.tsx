036969cd8c59c7663a5d340a0ce94c2dfd4699e0
khoirxz <khoirrurrohman@gmail.com>
2025-02-16 17:01:02 +0700

    change config

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 
 import ProjectSection from "./partials/Project";
 
-import { siteConfig } from "@/config/site";
 import { METAINFOPROPS } from "@/app/types/meta";
 import { Image } from "@heroui/image";
 import { EXPERIENCEPROPS } from "./types/experience";
